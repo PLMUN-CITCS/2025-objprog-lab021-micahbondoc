2a733c2832df6ba53accb83fdc49c33e64455320
Micah Bondoc <bondocmicahjhem_bscs@plmun.edu.ph>
2025-03-03 18:04:47 +0800

    Submitting OBJPROG Week 05 - Laboratory # 21

---
+++ WordCounter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        String sentence = getSentenceInput();
+        int wordCount = countWords(sentence);
+        System.out.println("The sentence has " + wordCount + " words.");
+    }
+
+    public static String getSentenceInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        scanner.close();
+        return sentence;
+    }
+
+    public static int countWords(String sentence) {
+        String[] words = sentence.trim().split("\\s+");
+        return words.length;
+    }
+}
